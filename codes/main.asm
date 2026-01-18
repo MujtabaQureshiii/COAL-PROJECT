d035173f5540cb065d64b9cc5f0fe7c7b985c87a
Mujtaba Qureshi <mujtabaqureshi009@gmail.com>
2026-01-18 02:57:56 -0800

    main.asm

--- codes/main.asm
+++ codes/main.asm
@@ -3,18 +3,5 @@
 
 
 .dat include irvive.lib
-.data
-bnwdbbdjbd
-dnwlkdnlkd
-y word ?
-
-x word ?
-diowndiondoiwwndionwdwwnoidnwoid
-y word 
-dwnd word
-
-z dword 10w;ld l;w d;lmww;dlmwl;dml;wdm
-
-dwndlp;kwnkl;d
-wbdwbdjkbwjkd
-vvyvhjvhj
+.bwdwbdbwd
+wdkndlknwdlknwdlknw
