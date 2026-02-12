a478b36a69e7306cb44e18e61eea37365be8ba3f
Mujtaba Qureshi <mujtabaqureshi009@gmail.com>
2026-02-12 03:38:10 -0800

    Update main.asm

--- codes/main.asm
+++ codes/main.asm
@@ -2,17 +2,4 @@ x word 5
 y word 6
 z word 7
 f wword 8
-.dat include irvive.lib
-dbwjkbd
-dbwjdb
-djlwndlwk
-dwwmdl;wd
-dwibvdkwd
-dwwdwdwd
-dwbdjbwd
-dwbvdhjbvwhjkd
-dwdwdwdwd
-dlkdnlknwdww
-move 
-dwndlkwd
-jauagjquayq
+=
